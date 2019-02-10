6922ec6e3848df68c3dc36548e5f61a6808be58c
haopXpycary <haopxpycary@foxmail.com>
2019-02-10 12:42:31 +0800

    改错
    1.成功修改trans.html储存方法
    2.改变字体样式
    3.改变传输方法以避免405报错

---
+++ tools.js
@@ -0,0 +1,17 @@
+console.log("Successed in importing tools.js.")
+function saveArray(Item,new_arrey,index) {
+  index = Number(index)
+  localStorage.setItem(Item, JSON.stringify(new_arrey));
+  return 0;
+}
+  
+function getArray(Item) {
+  return JSON.parse(localStorage.getItem(Item));
+}
+
+function addtoArray(Item,index,data) {
+  index = Number(index)
+  var new_arrey = JSON.parse(localStorage.getItem(Item));
+  new_arrey[index] = data
+  localStorage.setItem(Item, JSON.stringify(new_arrey));
+}
