e572af04065d00dbfdad471cd89b63462a8750f2
haopXpycary <haopxpycary@foxmail.com>
2019-02-17 18:54:18 +0800

    优化背景音乐的播放

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ window.addEventListener('popstate', function () {
     window.history.pushState(null, null, document.URL);
 });
 
-// if (! document.cookie) document.cookie = "path=/"
-
 //音量控制
 function lowVolume(self) {
-    if (document.referrer.length == 0) {
+    if (parent.document.referrer.length == 0) {
         if (self.volume > 0.3) self.volume = 0.3
+        console.log("Now the volume of background music is: ",self.volume.toString())
     }
 }
